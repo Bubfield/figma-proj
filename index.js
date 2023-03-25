082d1b4aa84aae0553788306a23ae106cccf3ba3
Bubfield <69983820+Bubfield@users.noreply.github.com>
2023-03-25 02:39:06 -0500

    added selected style to home link by default on page load

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ headerListItems.forEach((item) => {
   orangeBar.setAttribute("id", "orange-bar");
   item.append(orangeBar);
 
+  function addSelectedStyleToSelectedItem() {
+    item.style.opacity = 1;
+    orangeBar.style.opacity = 1;
+    selectedItem = item;
+  }
+
+  if (item.id === "home") {
+    addSelectedStyleToSelectedItem();
+    orangeBar.style.width = "1.8rem";
+  }
+
   item.addEventListener("click", () => {
     function removeSelectedStyleFromOldItem() {
-      if (selectedItem && selectedItem.id !== item.id) {
+      if (selectedItem.id !== item.id) {
         selectedItem.style.opacity = 0.7;
         let itemsOrangeBar = selectedItem.children[0];
         itemsOrangeBar.style.opacity = 0.7;
@@ -19,19 +30,12 @@ headerListItems.forEach((item) => {
 
     removeSelectedStyleFromOldItem();
 
-    function addSelectedStyleToSelectedItem() {
-      item.style.opacity = 1;
-      orangeBar.style.opacity = 1;
-      selectedItem = item;
-    }
-
     addSelectedStyleToSelectedItem();
   });
 
   item.addEventListener("mouseenter", () => {
     if (item !== selectedItem) {
       item.style.opacity = 0.9;
-      item.style.cursor = "pointer";
       orangeBar.style.width = "1.8rem";
     }
   });
